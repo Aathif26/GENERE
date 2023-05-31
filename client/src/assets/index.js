6e97fc1cbeeca9b0084eaec284cbe1077e00d260
Aathif26 <aathif0710@gmail.com>
2023-05-31 15:37:12 +0530

    first commit

---
+++ client/src/assets/index.js
@@ -0,0 +1,12 @@
+import download from './download.png';
+import logo from './logo.svg';
+import preview from './preview.png';
+import Capture from './Capture.svg';
+
+
+export {
+  download,
+  logo,
+  preview,
+  Capture
+};
